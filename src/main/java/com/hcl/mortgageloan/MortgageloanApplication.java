4cf8c807259ba1d3db704cc0322cccda9eb93d00
piyush <piyushr999@gmail.com>
2019-07-12 10:12:56 +0530

    Initial Commit

---
+++ src/main/java/com/hcl/mortgageloan/MortgageloanApplication.java
@@ -0,0 +1,13 @@
+package com.hcl.mortgageloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MortgageloanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MortgageloanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hcl/mortgageloan/MortgageloanApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hcl.mortgageloan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MortgageloanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
