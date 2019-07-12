ddad8ef00e943e129490129386c8ee3ee64100eb
piyush <piyushr999@gmail.com>
2019-07-12 13:40:53 +0530

    Change error messages

--- src/main/java/com/hcl/mortgageloan/validator/UserInputValidator.java
+++ src/main/java/com/hcl/mortgageloan/validator/UserInputValidator.java
@@ -25,11 +25,11 @@ public class UserInputValidator {
 	 */
 	public boolean validateInput(UserInputDto userInput, Double price) throws InvalidInputException {
 		if (Period.between(userInput.getDateOfBirth(), LocalDate.now()).getYears() < 25) {
-			throw new InvalidInputException("You are not allowd for loan. Minimum age should be 25");
+			throw new InvalidInputException("You are not eligible for loan. Minimum age should be 25");
 		} else if (userInput.getMonthlySalary() < 10000) {
-			throw new InvalidInputException("You are not allowd for loan. Minimum salary should be 10000");
+			throw new InvalidInputException("You are not eligible for loan. Minimum salary should be 10000");
 		} else if ((userInput.getPropAreaSize() * price) < 500000) {
-			throw new InvalidInputException("You are not allowd for loan. Minimum property value should be 500000");
+			throw new InvalidInputException("You are not eligible for loan. Minimum property value should be 500000");
 		}
 		return true;
 	}
