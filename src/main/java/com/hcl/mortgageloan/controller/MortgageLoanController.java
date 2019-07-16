4b2311d40722a5e6b1806cd72f53840dcd37409a
piyush <piyushr999@gmail.com>
2019-07-16 10:46:45 +0530

    change context path and request mappings

--- src/main/java/com/hcl/mortgageloan/controller/MortgageLoanController.java
+++ src/main/java/com/hcl/mortgageloan/controller/MortgageLoanController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hcl.mortgageloan.dto.UserInputDto;
@@ -19,6 +20,7 @@ import com.hcl.mortgageloan.service.MortgageLoanService;
  *
  */
 @RestController
+@RequestMapping("/mortgage")
 public class MortgageLoanController {
 
 	@Autowired
